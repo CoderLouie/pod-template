774e7b2448c3b5bba8536905a77331807fe74481
liyang <18616562401@163.com>
2021-07-09 23:44:14 +0800

    1. xxx

---
+++ templates/swift/Example/PROJECT/Modules/Main/NavigationViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NavigationViewController.swift
+//  PROJECT
+//
+//  Created by 李阳 on 2021/7/9.
+//  Copyright © 2021 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
