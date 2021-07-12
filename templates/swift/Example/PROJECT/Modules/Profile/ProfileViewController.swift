83e4950b67800df21d0c7f75047ca8707a933f94
liyang <18616562401@163.com>
2021-07-12 16:15:50 +0800

    1. 因为gitignore文件导致 ProfileViewController推送不上去

---
+++ templates/swift/Example/PROJECT/Modules/Profile/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  PROJECT
+//
+//  Created by USER_NAME on TODAYS_DATE.
+//  Copyright (c) TODAYS_YEAR PROJECT_OWNER. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: BaseViewController {
+  
+    override func setupViews() {
+        super.setupViews()
+    }
+    
+    
+}
